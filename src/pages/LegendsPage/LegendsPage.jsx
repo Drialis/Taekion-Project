7732e6162b75c0b651e95fc838f1ee93ebd48a8f
Paloma Rehecho <paloma_re@hotmail.com>
2024-10-05 23:14:21 +0100

    updated LegendsPage

--- src/pages/LegendsPage/LegendsPage.jsx
+++ src/pages/LegendsPage/LegendsPage.jsx
@@ -1,5 +1,54 @@
+import { Card, Col, Container, Row } from 'react-bootstrap'
+import './LegendsPage.css'
+
 const LegendsPage = () => {
 
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Title>
+                            MIGUEL ÁNGEL SANTIAGO RUIZ
+                        </Card.Title>
+                        <Card.Img>
+
+                        </Card.Img>
+                        <Card.Text>
+                            Maestro
+                        </Card.Text>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Title>
+                            MIGUEL ÁNGEL SANTIAGO RUIZ
+                        </Card.Title>
+                        <Card.Img>
+
+                        </Card.Img>
+                        <Card.Text>
+                            Maestro
+                        </Card.Text>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Title>
+                            MIGUEL ÁNGEL SANTIAGO RUIZ
+                        </Card.Title>
+                        <Card.Img>
+
+                        </Card.Img>
+                        <Card.Text>
+                            Maestro
+                        </Card.Text>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+
+    )
 }
 
 export default LegendsPage
